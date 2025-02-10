e9e5f42604fd2dd9ae2b0cab9d27230b7caff306
Diogo Carvalho <diogoscarvalho02@gmail.com>
2025-02-10 14:21:53 -0300

    Filtragem no mapa

--- app/app/(tabs)/(maps)/mapsme.tsx
+++ app/app/(tabs)/(maps)/mapsme.tsx
@@ -1,14 +1,30 @@
 import React, { useEffect, useState } from 'react';
-import MapView, { Region } from 'react-native-maps';
+import MapView, { Marker, Region } from 'react-native-maps';
 import { StyleSheet, View } from 'react-native';
 import { useAuth } from "../../../context/AuthProvider";
+import axios from 'axios';
+
+type ItemData = {
+  id_publicacao: string;
+  titulo_publicacao: string;
+  imagem_publicacao: string;
+  descricao_publicacao: string;
+  estado_publicacao: string;
+  cidade_publicacao: string;
+  id_usuario: string;
+  nome_usuario: string;
+  sobrenome_usuario: string;
+  latitude: string;
+  longitude: string;
+};
 
 export default function App() {
   const { user, getLocation } = useAuth();
   const [region, setRegion] = useState<Region | null>(null);
+  const [userPosts, setUserPosts] = useState<ItemData[]>([]); // Especifique o tipo como ItemData[]
 
   useEffect(() => {
-    const fetchLocation = async () => {
+    const fetchLocationAndPosts = async () => {
       const location = await getLocation();
       if (location) {
         setRegion({
@@ -17,11 +33,25 @@ export default function App() {
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421,
         });
+
+        try {
+          const response = await axios.get('publicacao', {
+            headers: {
+              Authorization: user?.token,
+            },
+          });
+
+          // Filtra as publicações do usuário conectado
+          const filteredPosts = response.data.filter((item: ItemData) => item.id_usuario === user?.id);
+          setUserPosts(filteredPosts);
+        } catch (error) {
+          console.error('ERROR', error);
+        }
       }
     };
 
-    fetchLocation();
-  }, [getLocation]);
+    fetchLocationAndPosts();
+  }, [getLocation, user]);
 
   return (
     <View style={styles.container}>
@@ -30,7 +60,19 @@ export default function App() {
           style={styles.map}
           initialRegion={region}
           showsUserLocation={true}
-        />
+        >
+          {userPosts.map((post: ItemData) => (
+            <Marker
+              key={post.id_publicacao}
+              coordinate={{
+                latitude: parseFloat(post.latitude),
+                longitude: parseFloat(post.longitude),
+              }}
+              title={post.titulo_publicacao}
+              description={post.descricao_publicacao}
+            />
+          ))}
+        </MapView>
       )}
     </View>
   );
